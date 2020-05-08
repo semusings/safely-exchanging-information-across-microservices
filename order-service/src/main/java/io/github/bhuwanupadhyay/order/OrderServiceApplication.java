b13ab900764521652c7ae41950e11ef1bb99a699
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-05-08 13:41:24 +0545

    docs: post

---
+++ order-service/src/main/java/io/github/bhuwanupadhyay/order/OrderServiceApplication.java
@@ -0,0 +1,14 @@
+package io.github.bhuwanupadhyay.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.schema.registry.client.EnableSchemaRegistryClient;
+
+@SpringBootApplication
+@EnableSchemaRegistryClient
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+}
